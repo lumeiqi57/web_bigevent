aaaebe51875047103785f40f9e49fe7fe008f936
lumeiqi57 <l1803782936@163.com>
2021-04-24 22:10:09 +0800

    完成了主页的开发功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -8,23 +8,23 @@ $.ajaxPrefilter(function(options) {
         // console.log(options.url);
 
     // 统一为有权限的接口 设置headers请求头
-//     if (options.url.indexOf('/my') !== -1) {
-//         options.headers = {
-//             Authorization: localStorage.getItem('token') || ''
-//         }
-//     }
+    if (options.url.indexOf('/my') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
 
 
 
-//     // 全局同一挂载 complete 回调函数
-//     options.complete = function(res) {
-//         if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-//             // 1 强制清空 token
-//             localStorage.removeItem('token')
-//                 // 2 强制跳转到登录页面
-//             location.href = '/login.html'
+    // 全局同一挂载 complete 回调函数
+    options.complete = function(res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1 强制清空 token
+            localStorage.removeItem('token')
+                // 2 强制跳转到登录页面
+            location.href = '/login.html'
 
-//         }
-//     }
+        }
+    }
 
-// })
+})
